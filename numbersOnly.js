f470f3e420144ff8e570f903221b6763faebdf09
Christopher D Green <christopher.duncan.green@gmail.com>
2017-01-01 14:02:56 -0800

    first commit

---
+++ numbersOnly.js
@@ -0,0 +1,12 @@
+function numbersOnly(arr){
+  var newArr=[];
+  for(var i=0;i<arr.length;i++){
+    if (typeof(arr[i])==='number'){
+      newArr.push(arr[i]);
+    }
+  }
+  console.log(newArr);
+}
+
+arr=[3,4,5,'P','orange'];
+numbersOnly(arr);
